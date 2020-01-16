6e08847936d6b17076eee6961db4b7b0d9791003
Jan <jan.lindheimer@hotmail.com>
2020-01-16 14:46:11 +0100

    Test Connection

---
+++ Others/__init__.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from logging import root
+
+
+class Application (tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there ["text"] = "Click me..."
+        self.hi_there ["command"] = self.say_hello
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="Quit", fg="red", command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hello(selfself):
+        print("Hello World!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.master.title('Hello...')
+app.mainloop()
