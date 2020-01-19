3ee668faec392063174643fe5c2854e95937bbe2
Jan <jan.lindheimer@hotmail.com>
2020-01-19 18:54:44 +0100

    Uebung 4 import

--- Excersise/u_03_objektorientiertes_programmieren/oop/src/main.py
+++ Excersise/u_03_objektorientiertes_programmieren/oop/src/main.py
@@ -4,13 +4,44 @@ Um Objekte vom Typ Customer oder Account zu erzeugen, müssen die Klassen aus de
 from customer import Customer
 from account import Account
 
+""""
+Es wird eine Instanz vom Typ "Customer" erzeugt. Die Init-Methode erwartet zwei Strings:
+1. Vorname
+2. Nachname
+Diese beiden Strings müssen beim erzeugen mitgegeben werden. Ansonsten wird eine Exception geschmissen.
+Wenn die Klammern leer bleiben wird eine Instanz von "Customer" ohne parameter erzeugt.
+"""
 c = Customer("Max", "Mustermann")
+"""
+In der Init-Methode von "Account" gibt es den Parameter "owner" vom Typ Customer
+"""
 a1 = Account(c)
 a1.display()
+"""
+Es wird der Wert 100 in die Variable Balance geschrieben und mit der "display"- Funktion auf der Konsole ausgegeben.
+"""
 a1.deposit(100.0)
+"""
+"display" ist eine eigene Methode in "Account" mit einer speziellen Formatierung
+"""
 a1.display()
+"""
+Es wird vom Objekt "a1" der Kontostand genommen und auf der Konsole ausgegeben
+"""
 print(a1.get_balance())
+"""
+Es wird die Referenz des Objektes angezeigt
+"""
 print(a1)
 print(c)
 
 # Ihr Code
+#Kunde erzeugen mit übergebenen Parametern
+customer1 = Customer("Hildegard", "Mustermann")
+#Konto erzeugen und eien Kunden eintragen -> customer1
+konto1 = Account(customer1)
+
+"""
+Wünschenswert wäre eine Historie, was für Aktionen am konto durchgeführt wurden.
+Außerdem soll die möglichkeit bestehen ein Dispo bis zu einer bestimmten Summe + Zinsen zu bekommen
+"""
